294e5527dfb85172421b9456494c5df216e7e34a
Giovanna <giovannagmoeller@gmail.com>
2022-12-08 09:53:01 -0300

    Aula 3.3

--- iQuiz-iOS/QuestaoViewController.swift
+++ iQuiz-iOS/QuestaoViewController.swift
@@ -28,9 +28,10 @@ class QuestaoViewController: UIViewController {
         configurarQuestao()
     }
     
-    func configurarQuestao() {
+    @objc func configurarQuestao() {
         labelTituloQuestao.text = questoes[numeroQuestao].titulo
         botoesResposta.forEach { botao in
+            botao.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
             botao.setTitle(questoes[numeroQuestao].respostas[botao.tag], for: .normal)
         }
     }
@@ -46,17 +47,21 @@ class QuestaoViewController: UIViewController {
     }
     
     @IBAction func botaoRespostaPressionado(_ sender: UIButton) {
-        verificarResposta(sender.tag)
-    }
-    
-    func verificarResposta(_ respostaUsuario: Int) {
-        if respostaUsuario == questoes[numeroQuestao].respostaCorreta {
+        if verificarRespostaCorreta(sender.tag) {
             pontuacao += 1
+            sender.backgroundColor = UIColor(red: 11/255, green: 161/255, blue: 53/255, alpha: 1.0)
+        } else {
+            sender.backgroundColor = UIColor(red: 211/255, green: 17/255, blue: 17/255, alpha: 1.0)
         }
+        
         if numeroQuestao < questoes.count - 1 {
             numeroQuestao += 1
-            configurarQuestao()
+            Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(configurarQuestao), userInfo: nil, repeats: false)
         }
+    }
+    
+    func verificarRespostaCorreta(_ respostaUsuario: Int) -> Bool {
+        return respostaUsuario == questoes[numeroQuestao].respostaCorreta
     }
     
     /*
