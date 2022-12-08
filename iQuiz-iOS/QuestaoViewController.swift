06802706bdc51ff6c203e5ead9a828c9aef54b52
Giovanna <giovannagmoeller@gmail.com>
2022-12-08 09:05:17 -0300

    Aula 2.2

---
+++ iQuiz-iOS/QuestaoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuestaoViewController.swift
+//  iQuiz-iOS
+//
+//  Created by Giovanna Moeller on 08/12/22.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurarLayout()
+    }
+    
+    func configurarLayout() {
+        navigationItem.hidesBackButton = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
